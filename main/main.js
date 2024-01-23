4d4987c93ace1837efa7d09885fae04f68333ee0
Yoo Jeong <04dbwjd@gmail.com>
2024-01-23 22:29:40 +0900

    commit

---
+++ main/main.js
@@ -0,0 +1,14 @@
+var b1 = document.querySelector("#button1");
+b1.addEventListener("click", function() {
+  window.open('../profile/profile.html', '_self')
+});
+
+var b2 = document.querySelector("#button2");
+b2.addEventListener("click", function() {
+  window.open('../music/music.html', '_self')
+});
+
+var b3 = document.querySelector("#button3");
+b3.addEventListener("click", function() {
+  window.open('../video/video.html', '_self')
+});
